54569dc73be0f7a9fbd38148e6cdd6043892fefc
6a209 <6a209@6a209s-iMac.local>
2019-03-10 16:21:32 +0800

    feat: init mysql

--- server/app/service/user.js
+++ server/app/service/user.js
@@ -3,7 +3,22 @@ const Service = require('egg').Service
 
 class UserService extends Service {
 
-    async  
+    async findUser(openId) {
+        const user = await this.app.mysql.get('user', {openId: openId})
+        return user
+    } 
+
+    async createUser(user) {
+        const result = await this.app.mysql.insert('user', user)
+        const insertSuccess = result.affectedRows === 1
+        return insertSuccess 
+    }
+
+    async updateUser(user) {
+        await this.app.mysql.update('user', user)
+        const insertSuccess = result.affectedRows === 1
+        return insertSuccess 
+    }
 }
 
 module.exports = UserService
--- server/config/plugin.js
+++ server/config/plugin.js
@@ -9,5 +9,11 @@ module.exports = {
   jwt : {
     enable: true,
     package: "egg-jwt"
-  }
+  },
+
+  mysql : {
+    enable: true,
+    package: 'egg-mysql'
+  },
+
 };
