95d23c17fc630cdb26c4c7d15f3d08f43a0c62b2
6a209 <6a209qt@gmail.com>
2019-04-09 00:40:39 +0800

    feat: udp broadcast

--- device/arduino/src/main.cpp
+++ device/arduino/src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <WiFiClient.h>
 #include <ESP8266WiFi.h>      
+#include <WiFiUdp.h>
 
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
@@ -10,14 +11,47 @@
 #include "ir.h"
 
 #include "custom_mqtt.h"
+#include <Ticker.h>
 
 String PK = String("IR");
 String DN = String("F92BA2A2-BE6D-45C1-9C95-A34E082E638D");
+String SK = String("c274761d-9a8c-42c2-992c-0974c512f4d4");
+
+// 
+String uid = "uid";
+
+#define UDP_PORT 9876
+char udpPackage[255];  // buffer for incoming packets
+WiFiUDP udp;
+Ticker UDPTicker;
+
+void sendUDPBroadcast() {
+  IPAddress ip = WiFi.localIP();
+  ip[3] = 255;
+  udp.beginPacket(ip, UDP_PORT);
+  char output[512];
+  DynamicJsonDocument doc(512);
+  doc["code"] = 200;
+  doc["msg"] = "ok";
+  doc["pk"] = PK.c_str(); 
+  doc["dn"] = DN.c_str();
+  doc["sk"] = SK.c_str();
+  // String sendData = PK + "&" + DN + "&" + SK;
+  serializeJson(doc, output);
+  udp.write(output);
+  udp.endPacket();
+}
+
+void parseBindData(String data) {
+  if (data.startsWith("uid")) {
+    uid = data.substring(3);
+  }
+}
 
 void onMqttConnect(bool sessionPresent) {
   // publish online
   String topic = String("device/online");
-  publishMsg(topic, PK + "&" + DN);
+  publishMsg(topic, PK + "&" + DN + "&" + SK);
 
   topic = PK + "&" + DN + "&" +  String("device/sendCommand");
   subscribe(topic);
@@ -55,8 +89,15 @@ void setup() {
   // String ds = F("YkmGJQ31zokDFLZOd6coS8KCklFumHdz");
   // aliyunIoTInit(pk, dn, ds);
   // aliyunIoTConnect();
+  udp.begin(UDP_PORT);
+
   initMqtt(onMqttConnect, onMqttMessage);
   initIR();
+
+  if (uid.equals("uid")) {
+    // not bind, send broadcast wait bind
+    UDPTicker.attach(5, sendUDPBroadcast);
+  }
 }
 
 void onReceiveIRData(uint16_t data[]) {
@@ -79,4 +120,10 @@ void onReceiveIRData(uint16_t data[]) {
 
 void loop() {
   receiveIRData(onReceiveIRData);
+  int packageSize = udp.parsePacket(); 
+  if (packageSize) {
+    int len = udp.read(udpPackage, 255);
+    udpPackage[len] = 0;
+    parseBindData(String(udpPackage));
+  }
 }
