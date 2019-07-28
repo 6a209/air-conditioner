a30b3c5358f28583685a07f4720feca92b4d44cc
6a209 <6a209qt@gmail.com>
2019-07-29 00:03:39 +0800

    feat: mobile bindBrand & update name & unbind

--- mobile_app/lib/device/api/device_detail.dart
+++ mobile_app/lib/device/api/device_detail.dart
@@ -1,17 +1,30 @@
 import '../../base/http_utils.dart';
 
-
 class DeviceDetailApi {
-  
-  final String DETAIL_PATH = "/device/detail";
-  final String COMMAND_PATH = "/device/command";
+  static final String DETAIL_PATH = "/device/detail";
+  static final String COMMAND_PATH = "/device/command";
+  static final String DELETE_PATH = "/device/unbind";
+  static final String UPDATE_NAME_PATH = "/device/updateName";
 
   getDetail(int deviceId) async {
-     return await IRHTTP().requestPost(DETAIL_PATH, data: {"deviceId": deviceId});
+    return await IRHTTP()
+        .requestPost(DETAIL_PATH, data: {"deviceId": deviceId});
   }
 
-  execCommand(int deviceId, Map status) {
+  execCommand(int deviceId, Map status) async {
     status["deviceId"] = deviceId;
-    return IRHTTP().requestPost(COMMAND_PATH, data: status);
+    return await IRHTTP().requestPost(COMMAND_PATH, data: status);
+  }
+
+  deleteDevice(int deviceId) async {
+    return await IRHTTP()
+        .requestPost(DELETE_PATH, data: {"deviceId": deviceId});
+  }
+
+  updateDeviceName(int deviceId, String name) async {
+    print(name);
+    print(deviceId);
+    return await IRHTTP().requestPost(UPDATE_NAME_PATH,
+        data: {"deviceId": deviceId, "name": name});
   }
 }
