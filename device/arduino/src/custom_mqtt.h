eee86385a2c64e216721824082d8530f58811b67
6a209 <6a209qt@gmail.com>
2019-08-04 17:51:53 +0800

    feat: device mqtt to use host & use custom clientid

--- device/arduino/src/custom_mqtt.h
+++ device/arduino/src/custom_mqtt.h
@@ -3,7 +3,7 @@
 typedef void (*OnMqttConnect)(bool session);
 typedef void (*OnMqttMessage)(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total);
 
-void initMqtt(OnMqttConnect connect, OnMqttMessage message);
+void initMqtt(String clientId, OnMqttConnect connect, OnMqttMessage message);
 
 void publishMsg(String& topic, String& payload);
 
